3e64ef13ea58e1a5df959ab3f73ecd6c7b8e0a0b
Abdoulaye Diallo <abdoulayediallo.dallein@outlook.com>
2023-07-17 10:50:50 +0200

    cours

---
+++ backend/src/cours/filters.py
@@ -0,0 +1,9 @@
+from rest_framework_gis.filterset import GeoFilterSet
+
+from .models import Cours 
+
+class CoursFilter(GeoFilterSet):
+
+    class Meta:
+        model = Cours
+        fields = ("id", "salle_id", "date_debut", "date_fin")
