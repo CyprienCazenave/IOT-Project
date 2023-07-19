63d7a65654c261b0e399d460d8878e071c476dbe
cazenave <cyprien@maplab.green>
2023-07-19 14:55:49 +0200

    add new routes api

--- backend/src/mqtt/mqtt.py
+++ backend/src/mqtt/mqtt.py
@@ -8,3 +8,11 @@ def mqtt_data_view(request):
 
     # Boucle de gestion des messages MQTT
     client.loop_start()
+
+    # Récupérer les données des capteurs
+    capteur1_data = donnees_capteurs.get("temperature", {})
+    capteur3_data = donnees_capteurs.get("lux", {})
+    capteur4_data = donnees_capteurs.get("level", {})
+    capteur5_data = donnees_capteurs.get("motion", {})
+
+    if donnees_capteurs ==
