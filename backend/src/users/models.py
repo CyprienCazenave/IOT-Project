c196e2c19af8e7b4d6e24ee85404ffa592ed72f0
cazenave <cyprien@maplab.green>
2023-07-19 12:38:00 +0200

    improve mqtt reception

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -2,17 +2,56 @@ import uuid
 
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 
-class User(TimeStampedModel):
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("The email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and save a regular User with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-    nom = models.CharField(max_length=255, null=True, blank=True, unique=True)
+    nom = models.CharField(max_length=255, null=True, blank=True)
     prenom = models.CharField(max_length=255, null=True, blank=True)
-    email = models.EmailField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=True, blank=True, unique=True)
     password = models.CharField(max_length=255, null=True, blank=True)
+    is_superuser = models.BooleanField(default=False)  # a superuser
+    is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'nom'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
